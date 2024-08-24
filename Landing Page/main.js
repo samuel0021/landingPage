6e1ec9b817127cf9833eb564869e0c6a798a1d1e
samuel0021 <139814578+samuel0021@users.noreply.github.com>
2024-08-24 19:48:52 -0300

    Add files via upload

---
+++ Landing Page/main.js
@@ -0,0 +1,4 @@
+window.addEventListener('scroll', function() {
+    const scrollPosition = window.scrollY || window.pageYOffset;
+    console.log(`Posição de rolagem atual: ${scrollPosition}px`);
+}); 
