886ece39d34102ef717699586dee93ce2a4219cc
samuel0021 <139814578+samuel0021@users.noreply.github.com>
2024-08-25 20:39:19 -0300

    Update main.js

--- Landing Page/main.js
+++ Landing Page/main.js
@@ -1,4 +1,19 @@
-window.addEventListener('scroll', function() {
-    const scrollPosition = window.scrollY || window.pageYOffset;
-    console.log(`Posição de rolagem atual: ${scrollPosition}px`);
-}); 
+
+// window.addEventListener('scroll', function() {
+//     const scrollPosition = window.scrollY || window.pageYOffset;
+//     conswole.log(`Posição de rolagem atual: ${scrollPosition}px`);
+// }); 
+
+
+function largura() {
+    let label = document.getElementById('lblLargura');
+    let larguraInterna = window.innerWidth;
+    /*label.innerText =*/ alert(larguraInterna + 'px');
+
+    let corBody = document.body;
+    corBody.style.backgroundColor = '';
+}
+
+// largura();
+
+window.addEventListener('scroll', largura());
